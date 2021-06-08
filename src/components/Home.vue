e259a174e1bae0ff0f289cd57e8b26c276c7fe5a
mark <1304101539@qq.com>
2021-06-08 23:39:10 +0800

    主页框架

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-      <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button></el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+       <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,5 +25,32 @@ export default {
 </script>
 
 <style Lang="less" scoped>
+  .home-container{
+    height: 100%
+  }
 
+ .el-header{
+   background-color: #373D41;
+   display: flex;
+   justify-content: space-between;
+   padding-left: 0;
+   align-items: center;
+   color: #fff;
+   font-size: 20px;
+ }
+ .el-header div{
+   display: flex;
+   align-items: center;
+  }
+  span {
+     margin-left: 15px;
+   }
+
+ .el-aside{
+   background-color: #333744;
+ }
+
+ .el-main{
+   background-color: #eaedf1;
+ }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Main, Aside } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
+
 Vue.prototype.$message = Message
